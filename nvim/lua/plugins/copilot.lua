2f3b11d4f7881142dcf87cf1ce3c7b3876bdc86c
Wesley Adriann <wesleyadriann@gmail.com>
2025-06-12 18:13:29 -0300

    adds alacritty nvim zellij configs
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,12 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      suggestion = { enabled = true },
+      panel = { enabled = true },
+    })
+  end,
+}
