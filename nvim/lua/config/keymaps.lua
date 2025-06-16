7ff9b67f32e9deaa1bd044933503b09c849cae4d
Wesley Adriann <wesleyadriann@gmail.com>
2025-06-16 17:22:39 -0300

    adds some keymaps and enables tutor
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -19,3 +19,12 @@ vim.keymap.set("i", "<A-S-Up>", "<Esc>:m .-2<CR>==gi", { desc = "Mover linha par
 -- Visual mode
 vim.keymap.set("v", "<A-S-Down>", ":m '>+1<CR>gv=gv", { desc = "Mover seleção para baixo (Alt+Shift+Down)" })
 vim.keymap.set("v", "<A-S-Up>", ":m '<-2<CR>gv=gv", { desc = "Mover seleção para cima (Alt+Shift+Up)" })
+
+vim.keymap.set("n", "<leader>t", "<C-w>h", { desc = "Toggle focus" })
+
+-- Vai para o buffer 1 com Ctrl+1
+vim.keymap.set("n", "<C-1>", ":buffer 1<CR>", { desc = "Ir para o Buffer 1" })
+-- Vai para o buffer 2 com Ctrl+2
+vim.keymap.set("n", "<C-2>", ":buffer 2<CR>", { desc = "Ir para o Buffer 2" })
+-- Vai para o buffer 3 com Ctrl+3
+vim.keymap.set("n", "<C-3>", ":buffer 3<CR>", { desc = "Ir para o Buffer 3" })
