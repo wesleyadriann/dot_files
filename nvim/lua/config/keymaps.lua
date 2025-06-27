22321e3901d8b0a739d76c191bf0e7778e316e7c
Wesley Adriann <wesleyadriann@gmail.com>
2025-06-27 16:51:40 -0300

    adds new keymaps
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -12,17 +12,21 @@ vim.keymap.set('n', '<C-S-a>', 'ggVG"+y', { desc = 'Selecionar tudo e copiar par
 vim.keymap.set('n', '<A-S-Down>', ':m .+1<CR>==', { desc = 'Mover linha para baixo (Alt+Shift+Down)' })
 vim.keymap.set('n', '<A-S-Up>', ':m .-2<CR>==', { desc = 'Mover linha para cima (Alt+Shift+Up)' })
 
+vim.keymap.set('n', '<A-S-j>', ':m .+1<CR>==', { desc = 'Mover linha para baixo (Alt+Shift+Down)' })
+vim.keymap.set('n', '<A-S-k>', ':m .-2<CR>==', { desc = 'Mover linha para cima (Alt+Shift+Up)' })
+
 -- Insert mode
 vim.keymap.set('i', '<A-S-Down>', '<Esc>:m .+1<CR>==gi', { desc = 'Mover linha para baixo (Alt+Shift+Down)' })
 vim.keymap.set('i', '<A-S-Up>', '<Esc>:m .-2<CR>==gi', { desc = 'Mover linha para cima (Alt+Shift+Up)' })
+
+vim.keymap.set('i', '<A-S-j>', '<Esc>:m .+1<CR>==gi', { desc = 'Mover linha para baixo (Alt+Shift+Down)' })
+vim.keymap.set('i', '<A-S-k>', '<Esc>:m .-2<CR>==gi', { desc = 'Mover linha para cima (Alt+Shift+Up)' })
 
 -- Visual mode
 vim.keymap.set('v', '<A-S-Down>', ":m '>+1<CR>gv=gv", { desc = 'Mover seleção para baixo (Alt+Shift+Down)' })
 vim.keymap.set('v', '<A-S-Up>', ":m '<-2<CR>gv=gv", { desc = 'Mover seleção para cima (Alt+Shift+Up)' })
 
-vim.keymap.set('n', '<leader>t', '<C-w>h', { desc = 'Toggle focus' })
-
-vim.keymap.set('n', '<C-p>', '<leader><space>', { desc = 'Buscar arquivos (Ctrl+P)' })
+vim.keymap.set('n', '<leader>h', '<C-w>h', { desc = 'Left window' })
 
 -- Control + i para abrir ações de código (correções rápidas)
 vim.keymap.set('n', '<C-.>', vim.lsp.buf.code_action, { desc = 'LSP Code Action (Correção Rápida)' })
