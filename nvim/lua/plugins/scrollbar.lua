df9c2ec2f28eaa4779176d1bfe63d7ac3f5f46e1
Wesley Adriann <wesleyadriann@gmail.com>
2025-08-13 18:16:04 -0300

    adds scrollbar
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- nvim/lua/plugins/scrollbar.lua
+++ nvim/lua/plugins/scrollbar.lua
@@ -1,5 +1,16 @@
 return {
   {
     'petertriho/nvim-scrollbar',
+    event = 'VeryLazy', -- carrega quando for necessário, ou altere para "BufReadPre"
+    config = function()
+      require('scrollbar').setup()
+    end,
+  },
+  {
+    'kevinhwang91/nvim-hlslens',
+    event = 'VeryLazy',
+    config = function()
+      require('scrollbar.handlers.search').setup()
+    end,
   },
 }
