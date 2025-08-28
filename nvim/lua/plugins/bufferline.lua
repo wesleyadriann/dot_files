e1bf795abc0a36802780ac3a1cf1b3f9dd3ee8bb
Wesley Adriann <wesleyadriann@gmail.com>
2025-08-28 19:11:40 -0300

    changes theme
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -3,6 +3,8 @@ return {
   opts = {
     options = {
       always_show_bufferline = true,
+      separator_style = 'slope',
+      auto_toggle_bufferline = true,
     },
   },
 }
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,15 @@
 return {
   -- { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
-  -- {
-  --   'folke/tokyonight.nvim',
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {},
-  -- },
   {
-    'projekt0n/github-nvim-theme',
-    name = 'github-theme',
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
   },
   {
     'LazyVim/LazyVim',
     opts = {
-      colorscheme = 'github_dark',
+      colorscheme = 'tokyonight-storm',
     },
   },
 }
