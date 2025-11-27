5346abb821715ad37aa24a14146ed07690b1169d
Wesley Adriann <wesleyadriann@gmail.com>
2025-11-27 20:52:05 -0300

    changes colorscheme
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,16 @@
 return {
   -- { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
-  {
-    'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
+  -- {
+  --   'folke/tokyonight.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  -- },
+  { 'bluz71/vim-moonfly-colors', name = 'moonfly', lazy = false, priority = 1000 },
   {
     'LazyVim/LazyVim',
     opts = {
-      colorscheme = 'tokyonight-storm',
+      colorscheme = 'moonfly',
     },
   },
 }
