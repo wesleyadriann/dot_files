6c46cd98ebb40fbee990e56a9d964db0e4bb6255
Wesley Adriann <wesleyadriann@gmail.com>
2025-12-05 20:04:00 -0300

    some configs in hyprland waybar and nvim
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -11,34 +11,29 @@ vim.keymap.set('n', '<C-S-a>', 'ggVG"+y', { desc = 'Selecionar tudo e copiar par
 vim.keymap.set('n', '<A-S-Down>', ':m .+1<CR>==', { desc = 'Mover linha para baixo (Alt+Shift+Down)' })
 vim.keymap.set('n', '<A-S-Up>', ':m .-2<CR>==', { desc = 'Mover linha para cima (Alt+Shift+Up)' })
 
-vim.keymap.set('n', '<A-S-j>', ':m .+1<CR>==', { desc = 'Mover linha para baixo (Alt+Shift+Down)' })
-vim.keymap.set('n', '<A-S-k>', ':m .-2<CR>==', { desc = 'Mover linha para cima (Alt+Shift+Up)' })
+vim.keymap.set('n', '<A-S-j>', ':m .+1<CR>==', { desc = 'Mover linha para baixo (Alt+Shift+j)' })
+vim.keymap.set('n', '<A-S-k>', ':m .-2<CR>==', { desc = 'Mover linha para cima (Alt+Shift+k)' })
 
 -- Insert mode
 vim.keymap.set('i', '<A-S-Down>', '<Esc>:m .+1<CR>==gi', { desc = 'Mover linha para baixo (Alt+Shift+Down)' })
 vim.keymap.set('i', '<A-S-Up>', '<Esc>:m .-2<CR>==gi', { desc = 'Mover linha para cima (Alt+Shift+Up)' })
 
-vim.keymap.set('i', '<A-S-j>', '<Esc>:m .+1<CR>==gi', { desc = 'Mover linha para baixo (Alt+Shift+Down)' })
-vim.keymap.set('i', '<A-S-k>', '<Esc>:m .-2<CR>==gi', { desc = 'Mover linha para cima (Alt+Shift+Up)' })
+vim.keymap.set('i', '<A-S-j>', '<Esc>:m .+1<CR>==gi', { desc = 'Mover linha para baixo (Alt+Shift+j)' })
+vim.keymap.set('i', '<A-S-k>', '<Esc>:m .-2<CR>==gi', { desc = 'Mover linha para cima (Alt+Shift+k)' })
 
 -- Visual mode
 vim.keymap.set('v', '<A-S-Down>', ":m '>+1<CR>gv=gv", { desc = 'Mover seleção para baixo (Alt+Shift+Down)' })
 vim.keymap.set('v', '<A-S-Up>', ":m '<-2<CR>gv=gv", { desc = 'Mover seleção para cima (Alt+Shift+Up)' })
 
-vim.keymap.set('v', '<A-S-j>', ":m '>+1<CR>gv=gv", { desc = 'Mover seleção para baixo (Alt+Shift+Down)' })
-vim.keymap.set('v', '<A-S-k>', ":m '<-2<CR>gv=gv", { desc = 'Mover seleção para cima (Alt+Shift+Up)' })
+vim.keymap.set('v', '<A-S-j>', ":m '>+1<CR>gv=gv", { desc = 'Mover seleção para baixo (Alt+Shift+j)' })
+vim.keymap.set('v', '<A-S-k>', ":m '<-2<CR>gv=gv", { desc = 'Mover seleção para cima (Alt+Shift+k)' })
 
 -- Control + . para abrir ações de código (correções rápidas)
-vim.keymap.set('n', '<A-.>', vim.lsp.buf.code_action, { desc = 'LSP Code Action (Correção Rápida)' })
-vim.keymap.set('v', '<A-.>', vim.lsp.buf.code_action, { desc = 'LSP Code Action (Correção Rápida)' })
-vim.keymap.set('n', '<C-.>', vim.lsp.buf.code_action, { desc = 'LSP Code Action (Correção Rápida)' })
-vim.keymap.set('v', '<C-.>', vim.lsp.buf.code_action, { desc = 'LSP Code Action (Correção Rápida)' })
+vim.keymap.set({ 'n', 'v' }, '<A-.>', vim.lsp.buf.code_action, { desc = 'LSP Code Action (Correção Rápida)' })
+vim.keymap.set({ 'n', 'v' }, '<C-.>', vim.lsp.buf.code_action, { desc = 'LSP Code Action (Correção Rápida)' })
 
-vim.keymap.set('n', '<S-Tab>', '<<', { desc = 'Desindentar linha' })
-vim.keymap.set('n', '<Tab>', '>>', { desc = 'Indentar linha' })
-
-vim.keymap.set('v', '<S-Tab>', '<<', { desc = 'Desindentar linha' })
-vim.keymap.set('v', '<Tab>', '>>', { desc = 'Indentar linha' })
+vim.keymap.set({ 'n', 'v' }, '<S-Tab>', '<<', { desc = 'Desindentar linha' })
+vim.keymap.set({ 'n', 'v' }, '<tab>', '>>', { desc = 'Indentar linha' })
 
 vim.keymap.set('n', '<leader>bn', '<cmd>enew<CR>', { desc = 'Novo buffer vazio' })
 
@@ -46,3 +41,5 @@ vim.keymap.set('n', 'x', '"_x')
 vim.keymap.set('v', 'x', '"_x')
 
 vim.keymap.set('n', '<leader>cz', ':LspRestart', { desc = 'Reiniciar lsp' })
+
+vim.keymap.set('n', '<C-y>', LazyVim.cmp.map({ 'ai_nes' }, '<tab>'), { desc = 'Sugestão de código (Sidekick)' })
